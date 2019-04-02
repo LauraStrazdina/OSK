6cf6289c6512de7076052166589e2c7f677ad11d
Laura <laura.strazdina@testdevlab.com>
2019-04-02 21:52:24 +0300

    FCFS

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
-alert("Hello! I am an alert box!!");
+//alert("Hello! I am an alert box!!");
+window.onload = function what(){
+
+var counter = document.getElementById('counter');
+var average_time = document.getElementById('average_time');
+
+var FCFS1 = parseInt(document.getElementById('1FCFS').innerHTML);
+var FCFS2 = parseInt(document.getElementById('2FCFS').innerHTML);
+var FCFS3 = parseInt(document.getElementById('3FCFS').innerHTML);
+var FCFS4 = parseInt(document.getElementById('4FCFS').innerHTML);
+var FCFS5 = parseInt(document.getElementById('5FCFS').innerHTML);
+var FCFS6 = parseInt(document.getElementById('6FCFS').innerHTML);
+
+var sum = FCFS1+FCFS2+FCFS3+FCFS4+FCFS5+FCFS6;
+
+counter.innerHTML = sum;
+average_time.innerHTML = sum/6;
+};
