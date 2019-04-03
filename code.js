5d85a0e73e0dff88ecd81d5a7be9634c23cc83da
Laura <laura.strazdina@testdevlab.com>
2019-04-03 18:57:44 +0300

    FCFS turpinājums

--- code.js
+++ code.js
@@ -1,18 +1,33 @@
 //alert("Hello! I am an alert box!!");
 window.onload = function what(){
 
-var counter = document.getElementById('counter');
-var average_time = document.getElementById('average_time');
+var submit = document.getElementById('submit');
 
-var FCFS1 = parseInt(document.getElementById('1FCFS').innerHTML);
-var FCFS2 = parseInt(document.getElementById('2FCFS').innerHTML);
-var FCFS3 = parseInt(document.getElementById('3FCFS').innerHTML);
-var FCFS4 = parseInt(document.getElementById('4FCFS').innerHTML);
-var FCFS5 = parseInt(document.getElementById('5FCFS').innerHTML);
-var FCFS6 = parseInt(document.getElementById('6FCFS').innerHTML);
+submit.onclick = function(){
 
-var sum = FCFS1+FCFS2+FCFS3+FCFS4+FCFS5+FCFS6;
+  var counter = document.getElementById('counter');
+  var average_time = document.getElementById('average_time');
+  var FCFS = document.getElementsByClassName('forCount');
 
-counter.innerHTML = sum;
-average_time.innerHTML = sum/6;
+
+  var sum=0;
+  for (i=0; i<FCFS.length ; i++){
+    sum= sum+parseInt(FCFS[i].value);
+  }
+  console.log(sum);
+
+
+
+  average_time.innerHTML = sum/FCFS.length;
+
+  var number = 0;
+  var interval = setInterval(() =>  {
+
+    counter.innerText =  number++;
+    if(number > sum) {
+      clearInterval(interval);
+    }
+  }, 1000);// repeat every 1 second
+};
+
 };
